fd14951e5c9f4c0b415477895025218cc6f63cf1
Nick Phillips <nicktgphillips@gmail.com>
2021-02-19 19:28:12 +0000

    Ignore some eslint rules, change Node version to 14

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   extends: ["airbnb-typescript-prettier"],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'react/require-default-props': 0,
     'react/no-array-index-key': 0,
+    'react/no-unused-prop-types': 0,
     'react/jsx-props-no-spreading': 0
   }
 };
