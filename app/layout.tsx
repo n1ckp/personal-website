dc7e3178880fa30d7d5838001822fbb98fa7668c
Nicholas Phillips <nicktgphillips@gmail.com>
2025-11-14 21:19:39 +0000

    Fix Rubik font import, remove unused app/head.tsx (all head tags now in app/layout.tsx)

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,15 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Rubik:300,400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         {children}
         <GoogleAnalytics />
